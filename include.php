da235f1bfa516671c218ecbc9eeca57b91edaa1a
Russkikh <russkikhmaksim221@gmail.com>
2020-09-09 19:37:24 +0700

    test attempt

---
+++ include.php
@@ -0,0 +1,20 @@
+<?php
+require_once './Lexer.php';
+require_once 'src/astClasses/AstNode.php';
+require_once 'src/astClasses/AstClass.php';
+require_once './src/LexicalAnalyzeClasses/CompleteToken.php';
+require_once './src/LexicalAnalyzeClasses/LexicalAnalyze.php';
+require_once './src/LexicalAnalyzeClasses/TokenFormation.php';
+require_once 'src/astClasses/AstPreprocessorDirectiveClass.php';
+require_once 'src/astClasses/AstRootClass.php';
+require_once 'src/astClasses/AstLibFuncClass.php';
+require_once 'src/astClasses/InstructionKeyWordClass.php';
+require_once 'src/astClasses/AstFuncAndIdClass.php';
+require_once 'src/astClasses/AstIfClass.php';
+require_once 'src/astClasses/AstWhileClass.php';
+require_once 'src/astClasses/AstAssigmentClass.php';
+
+
+
+
+
